e190b70b4204048b82ccc5d2dac59a0e7c30b374
smalltitslovr2 <smalltitslovr2@gmail.com>
2017-05-01 15:35:18 -0400

    Got basic image generation working

---
+++ js/export.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  var node = document.getElementById('phone-container');
+
+  domtoimage.toJpeg(node)
+    .then(function (dataUrl) {
+        var img = new Image();
+        img.src = dataUrl;
+        document.body.appendChild(img);
+    })
+    .catch(function (error) {
+        console.error('oops, something went wrong!', error);
+    });
+})
